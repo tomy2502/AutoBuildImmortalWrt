8ae69a4d5baa309cc445ba1e8c408b8ef27a5dd5
tomy2502 <56631961+tomy2502@users.noreply.github.com>
2025-05-02 22:54:19 +0800

    Update build.sh
    解决编译错误，https://github.com/wukongdaily/AutoBuildImmortalWrt/issues/66#issuecomment-2661289415

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -25,6 +25,8 @@ echo "[Tomy] $(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES iptables-nft"
+PACKAGES="$PACKAGES ip6tables-nft"
 PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-attendedsysupgrade-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
